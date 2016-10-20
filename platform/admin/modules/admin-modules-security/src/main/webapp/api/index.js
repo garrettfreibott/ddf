bcc65ea6cacc14ae0c1f6bed8c8517b56e7bdabf
Chris Badahdah <a.s.badahdah@gmail.com>
2016-10-20 09:39:09 -0700

    Fixed standard issue.

--- platform/admin/modules/admin-modules-security/src/main/webapp/api/index.js
+++ platform/admin/modules/admin-modules-security/src/main/webapp/api/index.js
@@ -3,7 +3,7 @@ export const list = () => new Promise((resolve, reject) => {
 })
 
 export const fetchStage = (id) =>
-  fetch('/admin/wizard/ldap/' + id, {credentials: 'same-origin'})
+  window.fetch('/admin/wizard/ldap/' + id, {credentials: 'same-origin'})
     .then((res) => res.json())
 
 export const submit = (stage, { method, url }) => {
