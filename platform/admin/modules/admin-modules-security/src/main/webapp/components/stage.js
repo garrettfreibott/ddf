a2084976ab17b9a0e35adc5eb8d632592e9a6f55
Chris Badahdah <a.s.badahdah@gmail.com>
2016-10-27 14:54:37 -0700

    Added success check circle!

--- platform/admin/modules/admin-modules-security/src/main/webapp/components/stage.js
+++ platform/admin/modules/admin-modules-security/src/main/webapp/components/stage.js
@@ -6,10 +6,40 @@ import Component from '../containers/component'
 import Loading from '../containers/loading'
 import Back from '../containers/back'
 
+import CheckCircle from 'material-ui/svg-icons/action/check-circle'
+import { green500 } from 'material-ui/styles/colors'
+
+const styles = {
+  container: {
+    position: 'relative'
+  },
+  success: {
+    position: 'absolute',
+    right: 0,
+    bottom: 0
+  },
+  check: {
+    height: 30,
+    width: 30
+  }
+}
+
+const Success = ({ isSuccessful }) => {
+  if (!isSuccessful) return null
+  return (
+    <div style={styles.container}>
+      <div style={styles.success}>
+        <CheckCircle style={styles.check} color={green500} />
+      </div>
+    </div>
+  )
+}
+
 export default ({ disabled, rootComponent = {}, actions = [], path = [] }) => (
   <Card style={{ width: 600, margin: 20, padding: 20, boxSizing: 'border-box' }}>
     <Loading />
     <Component path={[ ...path, 'rootComponent' ]} {...rootComponent} disabled={disabled} />
     <Back disabled={disabled} />
+    <Success isSuccessful={disabled} />
   </Card>
 )
